b2416f1b9917be08882d1f878053cc6bdec0b6e7
Tomasz Hodyra <t.hodyra@gmail.com>
2018-12-14 18:17:39 +0100

    new grammar

--- ModulASR/ASR.cs
+++ ModulASR/ASR.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using DBConnector.Model;
@@ -65,31 +66,9 @@ namespace ModulASR
         {
             if (e.Result != null)
             {
-               
-//                txtConfidence.Text = e.Result.Confidence.ToString("0.0000");
-                if (e.Result.Semantics != null && e.Result.Semantics.Count != 0)
+                if (e.Result.Semantics != null)
                 {
-                    Console.WriteLine(GetValue(e.Result.Semantics, "Orzeczenie"));
-
-//                    txtValue0.Text = GetValue(e.Result.Semantics, "ORZECZENIE");
-//                    txtValue.Text = txtValue0.Text;
-//                    txtConfidence0.Text = GetConfidence(e.Result.Semantics, "ORZECZENIE");
-//
-//                    txtValue1.Text = GetValue(e.Result.Semantics, "BEZOKOLICZNIK");
-//                    txtValue.Text += " " + txtValue1.Text;
-//                    txtConfidence1.Text = GetConfidence(e.Result.Semantics, "BEZOKOLICZNIK"); ;
-//
-//                    txtValue2.Text = GetValue(e.Result.Semantics, "LICZBA");
-//                    txtValue.Text += " " + txtValue2.Text;
-//                    txtConfidence2.Text = GetConfidence(e.Result.Semantics, "LICZBA");
-//
-//                    txtValue3.Text = GetValue(e.Result.Semantics, "DOPELNIENIE");
-//                    txtValue.Text += " " + txtValue3.Text;
-//                    txtConfidence3.Text = GetConfidence(e.Result.Semantics, "DOPELNIENIE");
-//
-//                    txtValue4.Text = GetValue(e.Result.Semantics, "PRZYDAWKA");
-//                    txtValue.Text += " " + txtValue4.Text;
-//                    txtConfidence4.Text = GetConfidence(e.Result.Semantics, "PRZYDAWKA");
+                    Console.WriteLine(e.Result.Text);
                 }
             }
         }
@@ -97,83 +76,11 @@ namespace ModulASR
 
         private Grammar CreateGrammar()
         {
-            SrgsRule ruleOrzeczenie = new SrgsRule("Orzeczenie");
-            SrgsOneOf orzeczenie = new SrgsOneOf(uberGrammar.orzeczenieDictionary);
-            ruleOrzeczenie.Add(orzeczenie);
-            //            CreateDescriptionRules(ruleOrzeczenie, 0, 0);
-
-            SrgsRule ruleBezokolicznik = new SrgsRule("Bezokolicznik");
-            SrgsOneOf bezokolicznik = new SrgsOneOf(uberGrammar.bezokolicznikDictionary);
-            ruleBezokolicznik.Add(bezokolicznik);
-            //            CreateDescriptionRules(ruleBezokolicznik, 0, 0);
-
-            SrgsRule ruleCarType = new SrgsRule("Typ_pojazdu");
-            SrgsOneOf carType = new SrgsOneOf(uberGrammar.carTypeDictionary);
-            ruleCarType.Add(carType);
-            //            CreateDescriptionRules(ruleCarType, 0, 0);
-
-            SrgsRule ruleWyrazeniePrzyimkowe1 = new SrgsRule("Wyrazenie_przyimkowe1");
-            SrgsOneOf wyrazeniePrzyimkowe1 = new SrgsOneOf(uberGrammar.wyrazeniePrzyimkowe1Dictionary);
-            ruleWyrazeniePrzyimkowe1.Add(wyrazeniePrzyimkowe1);
-            //            CreateDescriptionRules(ruleWyrazeniePrzyimkowe1, 0, 0);
-
-            SrgsRule ruleAddressStreet = new SrgsRule("Adres_ulica");
-            SrgsOneOf addressStreet = new SrgsOneOf(uberGrammar.addressStreetDictionary);
-            ruleAddressStreet.Add(addressStreet);
-            //            CreateDescriptionRules(ruleAddressStreet, 0, 0);
-
-            SrgsRule ruleAddressNumber = new SrgsRule("Adres_numer");
-            SrgsOneOf addressNumber = new SrgsOneOf(uberGrammar.addressNumberDictionary);
-            ruleAddressNumber.Add(addressNumber);
-            //            CreateDescriptionRules(ruleAddressNumber, 0, 0);
-
-            SrgsRule ruleWyrazeniePrzyimkowe2 = new SrgsRule("Wyrazenie_przyimkowe2");
-            SrgsOneOf wyrazeniePrzyimkowe2 = new SrgsOneOf(uberGrammar.wyrazeniePrzyimkowe2Dictionary);
-            ruleWyrazeniePrzyimkowe2.Add(wyrazeniePrzyimkowe2);
-            //            CreateDescriptionRules(ruleWyrazeniePrzyimkowe2, 0, 0);
-
-            SrgsRule ruleTimeHour = new SrgsRule("Godzina");
-            SrgsOneOf timeHour = new SrgsOneOf(uberGrammar.timeHourDictionary);
-            ruleTimeHour.Add(timeHour);
-            //            CreateDescriptionRules(ruleTimeHour, 0, 0);
-
-            SrgsRule ruleTimeMinute = new SrgsRule("Minuta");
-            SrgsOneOf timeMinute = new SrgsOneOf(uberGrammar.timeMinuteDictionary);
-            ruleTimeMinute.Add(timeMinute);
-            //            CreateDescriptionRules(ruleTimeMinute, 0, 0);
-
-
-            // utwórz korzeń.
-            SrgsRule rootRule = new SrgsRule("rootUber");
-            rootRule.Scope = SrgsRuleScope.Public;
-
-
-            rootRule.Add(new SrgsRuleRef(ruleOrzeczenie, "Orzeczenie"));
-            rootRule.Add(new SrgsRuleRef(ruleBezokolicznik, "Bezokolicznik"));
-            rootRule.Add(new SrgsRuleRef(ruleCarType, "Typ_pojazdu"));
-            rootRule.Add(new SrgsRuleRef(ruleWyrazeniePrzyimkowe1, "Wyrazenie_przyimkowe1"));
-            rootRule.Add(new SrgsRuleRef(ruleAddressStreet, "Adres_ulica"));
-            rootRule.Add(new SrgsRuleRef(ruleAddressNumber, "Adres_numer"));
-            rootRule.Add(new SrgsRuleRef(ruleWyrazeniePrzyimkowe2, "Wyrazenie_przyimkowe2"));
-            rootRule.Add(new SrgsRuleRef(ruleTimeHour, "Godzina"));
-            rootRule.Add(new SrgsRuleRef(ruleTimeMinute, "Minuta"));
-
-
-            SrgsDocument docUber = new SrgsDocument();
-            docUber.Culture = pRecognitionLanguage;
-            docUber.Rules.Add(new SrgsRule[]
-                { rootRule, ruleOrzeczenie, ruleBezokolicznik, ruleCarType, ruleWyrazeniePrzyimkowe1, ruleAddressStreet, ruleAddressNumber, ruleWyrazeniePrzyimkowe2, ruleTimeHour, ruleTimeMinute }
-             );
-
-            docUber.Root = rootRule;
-
-            System.Xml.XmlWriter writer = System.Xml.XmlWriter.Create("srgsDocument.xml");
-            docUber.WriteSrgs(writer);
-            writer.Close();
-
-            Grammar gramatyka = new Grammar(docUber, "rootUber");
-
-            return gramatyka;
+            GrammarBuilder builder = new GrammarBuilder();
+            string fileName = @"c:\Users\thody\IdeaProjects\swp_projekt\ModulASR\grammar.xml";
+            Grammar citiesGrammar = new Grammar(new FileStream(fileName, FileMode.Open));
+            //citiesGrammar.Name = "Stream Cities Grammar";
+            return citiesGrammar;
         }
     }
 }
